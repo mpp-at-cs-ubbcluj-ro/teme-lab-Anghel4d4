fde9bfc43d81bbf294c38213f94d2632cbf46356
Brabus <danuta4d4@gmail.com>
2025-02-25 12:33:05 +0200

    SecondAttempt

--- Start.java
+++ Start.java
@@ -1,5 +1,5 @@
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Salut! Acesta este un mesaj tipărit la consolă.");
+        System.out.println("*****.");
     }
 }
